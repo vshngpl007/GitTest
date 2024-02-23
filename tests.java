be8dd4094e01eda863c30461cf7355c48f941c97
vshngpl007 <88106155+vshngpl007@users.noreply.github.com>
2024-02-23 10:15:37 +0530

    Update tests.java

--- tests.java
+++ tests.java
@@ -1,1 +1,29 @@
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FacebookLogin {
+    public static void main(String[] args) {
+        // Set the path to the chromedriver executable
+
+
+        // Create a new instance of the Chrome driver
+        WebDriver driver = new ChromeDriver();
+
+        // Open Facebook
+        driver.get("https://www.facebook.com");
+
+        // Optional: Maximize the browser window
+        driver.manage().window().maximize();
+
+        // Optional: Add some wait time to see the opened page
+        try {
+            Thread.sleep(3000); // Sleep for 3 seconds (you might want to use WebDriverWait in real scenarios)
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Close the browser
+        driver.quit();
+    }
+}
